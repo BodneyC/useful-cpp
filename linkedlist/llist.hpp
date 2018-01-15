425859ff427f1c3e7cf5b9f7a094f1c03f2190ab
Benjamin Carrington <benjcarrington@gmail.com>
2018-01-15 18:54:40 +0000

    Fixed removal of node via value

--- linkedlist/llist.hpp
+++ linkedlist/llist.hpp
@@ -1,13 +1,17 @@
-#ifndef LLIST
-#define LLIST
+#ifndef _LLIST_
+#define _LLIST_
 
 #include <iostream>
 
 template<class Type>
-struct Node
+class Node
 {
+public:
     Type value_t;
     Node<Type>* next;
+
+    Node(Type _value_t): value_t(_value_t), next(NULL) {}
+    ~Node() = default;
 };
 
 template<class Type>
@@ -17,7 +21,7 @@ public:
     Node<Type>* head;
     Node<Type>* tail;
 
-    LList(Node<Type>* initNode): head(initNode), tail(initNode) {}
+    LList(Node<Type>* initNode);
     LList(): head(NULL), tail(NULL) {}
     LList(Type value_t);
     ~LList();
@@ -27,18 +31,24 @@ public:
     void addHead(Type value_t);
     void addTail(Type value_t);
     void removeVal(Type value_t);
-    void removeNode(Node<Type> node);
+    void removeNode(Node<Type>* node);
     void print(std::ostream& stream = std::cout) const;
 };
+
+template<class Type>
+LList<Type>::LList(Node<Type>* node)
+{
+    head = tail = node;
+    tail->next = NULL;
+}
 
 template<class Type>
 LList<Type>::LList(Type value_t)
 {
     Node<Type> tmp = new Node<Type>();
-    head = tmp;
+    tmp->value_t = value_t;
 
-    head->value_t = value_t;
-    tail = head;
+    head = tail = tmp;
     tail->next = NULL;
 }
 
@@ -59,15 +69,14 @@ void LList<Type>::addHead(Node<Type>* node)
 {
     node->next = head;
     head = node;
-    if(!(head->next))
+    if(head->next == NULL)
         head->next = tail;
 }
 
 template<class Type>
 void LList<Type>::addHead(Type value_t)
 {
-    Node<Type>* tmp = new Node<Type>();
-    tmp->value_t = value_t;
+    Node<Type>* tmp = new Node<Type>(value_t);
 
     tmp->next = head;
     head = tmp;
@@ -85,8 +94,7 @@ void LList<Type>::addTail(Node<Type>* node)
 template<class Type>
 void LList<Type>::addTail(Type value_t)
 {
-    Node<Type>* tmp = new Node<Type>();
-    tmp->value_t = value_t;
+    Node<Type>* tmp = new Node<Type>(value_t);
 
     tail->next = tmp;
     tail = tmp;
@@ -95,17 +103,20 @@ void LList<Type>::addTail(Type value_t)
 template<class Type>
 void LList<Type>::removeVal(Type value_t)
 {
-    Node<Type>* tmp = &head;
+    Node<Type>* tmp = head;
 
-    while((*tmp)->next)
-        if((*tmp)->value_t = value_t)
-            removeNode(*tmp);
+    while(tmp->next) {
+        if(tmp->value_t == value_t)
+            removeNode(tmp);
+        
+        tmp = tmp->next;
+    }
 }
 
 template<class Type>
-void LList<Type>::removeNode(Node<Type> node)
+void LList<Type>::removeNode(Node<Type>* node)
 {
-    Node<Type>* tmp = &head;
+    Node<Type>** tmp = &head;
 
     while((*tmp) != node)
         tmp = &(*tmp)->next;
@@ -116,14 +127,15 @@ void LList<Type>::removeNode(Node<Type> node)
 template<class Type>
 void LList<Type>::print(std::ostream& stream) const
 {
-    Node<Type>* tmp = new Node<Type>;
+    Node<Type>* tmp = new Node<Type>(0);
     tmp = head;
     int i = 0;
 
     stream << "Printing from head:" << std::endl;
 
     while(tmp) {
-        stream << "Node: " << i << "\tValue: " << tmp->next;
+        stream << "Node: " << i << "\tValue: " << tmp->value_t << std::endl;
+        tmp = tmp->next;
         i++;
     }
 }
--- linkedlist/test.cpp
+++ linkedlist/test.cpp
@@ -2,10 +2,24 @@
 
 int main(int argc, char** argv)
 {
-    //Node<int>* n1 = new Node<int>;
-    //n1->value = 7;
+    Node<int>* n1 = new Node<int>(0);
 
-    LList<int> llist(new Node<int>);
-    std::cout << "FISH";
+    LList<int> llist(n1);
+
+    for(int i = 1; i < 10; i++)
+        llist.addTail(i);
+
+    for(int i = 10; i < 20; i++)
+        llist.addTail(new Node<int>(i));
+
+    llist.removeVal(5);
+
+    Node<int>* rem = new Node<int>(10);
+    llist.addHead(rem);
+
+    llist.removeNode(rem);
+
+    llist.print();
+
     return 0;
 }
