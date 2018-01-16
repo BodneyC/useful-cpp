382f275b056c8adc5b2a829477da783fa0f02d5c
Benjamin Carrington <benjcarrington@gmail.com>
2018-01-16 18:06:09 +0000

    Stack update

---
+++ stack/stack.hpp
@@ -0,0 +1,52 @@
+#ifndef _STACK_
+#define _STACK_
+
+template<class Type>
+class Stack
+{
+private:
+    int width;
+    int point;
+    Type* arr;
+
+public:
+    Stack(int _width = 50);
+    ~Stack() = default;
+
+    bool push(const Type &entry);
+    Type pop(void);
+    int getPoint(void);
+};
+
+template<class Type>
+Stack<Type>::Stack(int _width): width(_width), point(0)
+{
+    arr = new Type[width];
+}
+
+template<class Type>
+bool Stack<Type>::push(const Type &entry)
+{
+    if(point == width - 1)
+        return false;
+
+    arr[point++] = entry;
+    return true;
+}
+
+template<class Type>
+Type Stack<Type>::pop(void)
+{
+    if(point >= 0)
+        return arr[--point];
+    
+    return 0;
+}
+
+template<class Type>
+int Stack<Type>::getPoint(void)
+{
+    return point;
+}
+
+#endif
---
+++ stack/test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "stack.hpp"
+
+int main(int argc, char** argv)
+{
+    Stack<int> stack; 
+
+    stack.push(5);
+    stack.push(6);
+    stack.push(7);
+
+    std::cout << stack.getPoint() << std::endl;
+    
+    int tmp = stack.getPoint();
+    for(int i = 0; i < tmp; i++) {
+        std::cout << stack.pop() << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
